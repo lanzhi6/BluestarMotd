25f1e2eb02a24ed8e5274b940b7232edeef57703
lanzhi6 <lan_zhi@outlook.com>
2022-09-25 14:46:47 +0800

    [1.0.1]添加配置文件变更检测

--- common/src/main/java/me/lanzhi/bluestarmotd/BluestarMotdPlugin.java
+++ common/src/main/java/me/lanzhi/bluestarmotd/BluestarMotdPlugin.java
@@ -2,11 +2,11 @@ package me.lanzhi.bluestarmotd;
 
 import me.lanzhi.api.Bluestar;
 import me.lanzhi.api.config.YamlFile;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.lang.reflect.Field;
 
 public final class BluestarMotdPlugin extends JavaPlugin
 {
@@ -36,7 +36,9 @@ public final class BluestarMotdPlugin extends JavaPlugin
     {
         this.saveDefaultConfig();
         new File(this.getDataFolder(),"icon").mkdirs();
-        config=YamlFile.loadYamlFile(new File(this.getDataFolder(),"config.yml"));
+        config=YamlFile.loadYamlFile(new File(this.getDataFolder(),"config.yml"),
+                                     true,
+                                     ChatColor.GREEN+"[BluestarMotd]配置文件变更已加载");
         Bluestar.getCommandManager().registerPluginCommand("bluestarmotd",this,new MainCommand(this));
         VersionManager.versionManager.init(new InfoGetter(this));
     }
